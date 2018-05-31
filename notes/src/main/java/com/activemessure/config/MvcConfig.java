f6c3240c843c5084963edb59e3d7aa460c5e98af
LauSing <lausing2007@gmail.com>
2018-05-31 16:30:18 -0700

    added basic HTTP Auth

---
+++ notes/src/main/java/com/activemessure/config/MvcConfig.java
@@ -0,0 +1,33 @@
+package com.activemessure.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class MvcConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private AuthenticationEntryPoint authEntryPoint;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+				.anyRequest().authenticated()
+				.and().httpBasic()
+				.authenticationEntryPoint(authEntryPoint);
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("am123").password("password").roles("USER");
+	}
+
+}
